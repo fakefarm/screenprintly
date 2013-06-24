082d65739da3f64ca82ecb796bd9db38ad57a1da
Dave Woodall <dave@hireDave.me>
2013-06-24 08:50:38 -0600

    Setup Printer

---
+++ spec/models/printer_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Printer do
+  it 'has a valid email'
+  it 'has a valid phone number'
+  it 'has a contact person'
+  it 'has a present address'
+
+  context 'status' do
+    it 'is pending until activated'
+  end
+end
