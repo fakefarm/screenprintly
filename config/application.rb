b4899a60d93b5e0be6e62b81aa169ac3bf97addf
Dave Woodall <dave@hireDave.me>
2013-07-02 08:39:52 -0600

    stop auto generating asset files

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ end
 
 module Screenprintly
   class Application < Rails::Application
+
+    config.generators.stylesheets = false
+    config.generators.javascript  = false
+    config.generators.helper      = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
