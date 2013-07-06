47e064cbefc68a711f7336ff692c45d05ff3eae4
Dave Woodall <dave@hireDave.me>
2013-07-06 09:27:31 -0600

    Update test for root view

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -5,15 +5,19 @@ feature 'Root' do
   scenario 'has app overview information' do
     visit root_path
     page.should have_text("Need T-shirts?")
-    page.should have_text("Get quotes from local printers")
-    page.should have_link("Request your city")
-    page.should have_link("List your shop")
+    page.should have_text("Find the best screen printer")
+    page.should have_text("Screen printers by city")
   end
 
-  scenario 'has list of cities' do
+  scenario 'has links to each city' do
+    create(:city, name: "Orlando")
+    create(:city, name: "Boston")
     visit root_path
-    page.should have_text("Participating cities")
+    page.should have_link("Orlando")
+    page.should have_link("Boston")
   end
+
+
 end
 
 
