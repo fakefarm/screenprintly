a2a476c4b484327989c13a5916712557a39bea7f
Dave Woodall <dave@hireDave.me>
2013-07-09 07:36:15 -0600

    Test for print_price#new

--- spec/features/print_price_spec.rb
+++ spec/features/print_price_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 feature 'Print pricing' do
+  scenario 'has new price tier page' do
+    create(:city)
+    printer = create(:printer)
+    visit printer_path(printer)
+    click_link 'Add a price tier'
+    page.should have_text("Add a new price tier")
+  end
 end
