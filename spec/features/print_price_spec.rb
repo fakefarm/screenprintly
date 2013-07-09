85cd6d91975a6ca52db02e1c0a2c18229340cf7e
Dave Woodall <dave@hireDave.me>
2013-07-09 08:21:06 -0600

    Feature test print_price form exists

--- spec/features/print_price_spec.rb
+++ spec/features/print_price_spec.rb
@@ -8,4 +8,21 @@ feature 'Print pricing' do
     click_link 'Add a price tier'
     page.should have_text("Add a new price tier")
   end
+
+  scenario 'can be added by filling out the form' do
+    create(:city)
+    printer = create(:printer)
+    visit printer_path(printer)
+    click_link 'Add a price tier'
+    select('12', :from => 'Min print qty')
+    select('23', :from => 'Max print qty')
+    select('standard', :from => 'Pallet type')
+    select('plastisol', :from => 'Ink type')
+    fill_in "Base color", with: 234
+    fill_in "One color", with: 234
+    fill_in "Two color", with: 234
+    fill_in "Three color", with: 234
+    click_button "Create price tier"
+    page.should have_text('PrintPrices#index')
+  end
 end
