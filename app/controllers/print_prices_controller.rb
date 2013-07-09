6b816706ebd92be9ec0782141efae47fc246f01b
Dave Woodall <dave@hireDave.me>
2013-07-09 08:21:49 -0600

    print_price form code to make feature test pass

--- app/controllers/print_prices_controller.rb
+++ app/controllers/print_prices_controller.rb
@@ -6,6 +6,16 @@ class PrintPricesController < ApplicationController
   end
 
   def new
+    @print_price = PrintPrice.new
+  end
+
+  def create
+    @print_price = PrintPrice.create(params[:print_price])
+    if @print_price.save
+      redirect_to print_prices_path
+    else
+      render 'new'
+    end
   end
 
   def edit
