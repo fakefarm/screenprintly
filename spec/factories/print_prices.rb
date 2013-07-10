d3b1453d797866fbf44544ba1bf0c675fa050ef1
Dave Woodall <dave@hireDave.me>
2013-07-10 10:32:41 -0600

    print_price factory

--- spec/factories/print_prices.rb
+++ spec/factories/print_prices.rb
@@ -1,4 +1,17 @@
 FactoryGirl.define do
   factory :print_price do
+    min_print_qty 12
+    max_print_qty 23
+    pallet_type "standard"
+    ink_type "plastisol"
+    base_color 23
+    one_color 23
+    two_color 23
+    three_color 23
+    four_color 23
+    five_color 23
+    six_color 23
+    seven_color 23
+    eight_color 23
   end
 end
