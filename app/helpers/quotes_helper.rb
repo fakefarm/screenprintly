ae81d34c23f6288ee1f889bb7e392514543bdc49
Dave Woodall <dave@hireDave.me>
2013-07-26 11:21:09 -0600

    Helper methods

---
+++ app/helpers/quotes_helper.rb
@@ -0,0 +1,14 @@
+module QuotesHelper
+  def color_options
+    [
+      ['1 color', 'one_color'],
+      ['2 colors', 'two_color'],
+      ['3 colors', 'three_color'],
+      ['4 colors', 'four_color'],
+      ['5 colors', 'five_color'],
+      ['6 colors', 'six_color'],
+      ['7 colors', 'seven_color'],
+      ['8 colors', 'eight_color']
+    ]
+  end
+end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,36 @@
 class Quote < ActiveRecord::Base
   attr_accessible :number_of_colors, :quantity
+
+  def quote_quantity(qty)
+    if (1..11).include?(qty)
+      1
+    elsif (12..23).include?(qty)
+      2
+    elsif (24..35).include?(qty)
+      3
+    elsif (36..47).include?(qty)
+      4
+    elsif (48..71).include?(qty)
+      5
+    elsif (72..143).include?(qty)
+      6
+    elsif (144..287).include?(qty)
+      7
+    elsif (288..575).include?(qty)
+      8
+    elsif (576..1171).include?(qty)
+      9
+    elsif (1172..2343).include?(qty)
+      10
+    elsif (2344..4687).include?(qty)
+      11
+    elsif (4688..9375).include?(qty)
+      12
+    elsif (9376..Inf).include?(qty)
+      13
+    end
+  end
 end
+
+
+
