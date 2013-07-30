3ccae5e9ec4d4c69e11d2b558fa19c9d9351a805
Dave Woodall <dave@hireDave.me>
2013-07-30 14:29:49 -0600

    Printer Profile jquery

---
+++ app/assets/javascripts/printer.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  $('#about').click(function(){
+    $('.printer-content').children().hide();
+    $('#about-content').toggle();
+  });
+
+  $('#customer-service').click(function(){
+    $('.printer-content').children().hide();
+    $('#customer-service-content').toggle();
+  });
+
+  $('#services').click(function(){
+    $('.printer-content').children().hide();
+    $('#services-content').toggle();
+  });
+
+  $('#terms').click(function(){
+    $('.printer-content').children().hide();
+    $('#terms-content').toggle();
+  });
+
+  $('#price-list').click(function(){
+    $('.printer-content').children().hide();
+    $('#price-list-content').toggle();
+  });
+
+  $('#garment-list').click(function(){
+    $('.printer-content').children().hide();
+    $('#garment-list-content').toggle();
+  });
+
+  $('#summary').click(function(){
+    $('.printer-content').children().hide();
+    $('#summary-content').show();
+  });
+});
