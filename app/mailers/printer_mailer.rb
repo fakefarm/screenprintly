9db40047196f20fa62b1c2bb10ebb53581dbdcad
Dave Woodall <dave@hireDave.me>
2013-08-19 15:37:38 -0600

    Email style changes

--- app/mailers/printer_mailer.rb
+++ app/mailers/printer_mailer.rb
@@ -1,8 +1,8 @@
 class PrinterMailer < ActionMailer::Base
-  default from: "dmwoodall@gmail.com"
+  default from: "dave@screenprintly.com"
 
   def new_quote(printer)
     @printer = printer
-    mail(to: @printer.email, subject: "New Quote!")
+    mail(to: @printer.email, subject: "New Quote from screenPrintly!")
   end
 end
